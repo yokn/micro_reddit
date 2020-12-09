a4566b2243274c079a2d810fb6922a68cfe48db9
yokn <yokn@protonmail.com>
2020-12-09 13:34:44 +0300

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true
+  validates :user_id, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :posts
+  validates :username, presence: true, length: { in: 3..32 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 16..64 }
+end
